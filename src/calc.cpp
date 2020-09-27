b764a307d6de85ad8f2f53a75b5bc14d6547d9b7
Lauri Wefer <lauri22@online.de>
2020-09-27 21:37:14 +1300

    Added root calculations and functionality to display a manual.

--- src/calc.cpp
+++ src/calc.cpp
@@ -1,11 +1,44 @@
 #include <iostream> // Library for I/O.
 #include <string> // Library for string functions.
-#include <cmath> // Library for std::powl.
-using namespace std; // Set namespace.
+#include <cmath> // Library for extended math functions.
+using namespace std;
 
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
+const char *manual =
+		" + => Addition\n"
+		" - => Subtraction\n"
+		" * => Multiplication\n"
+		" / => Division\n"
+		" ^ => Exponentiate by <>\n"
+		"r2 => Square root of <>\n"
+		"r3 => Cubic root of <>\n"
+		" q => Quit\n"
+		" m => This page\n";
 string raw;
 long double a, b, c;
+
+string root(bool cube) {
+	if (cube == true) {
+		cout << "Enter value to get the cube root of: ";
+		getline(cin, raw);
+		try { // Test for invalid_argument.
+			a = stold(raw);
+		} catch (invalid_argument&) {
+			return "Invalid."; // invalid_argument exception? Return "Invalid".
+		}
+		c = cbrt(a); // Get cubic root of a.
+	} else {
+		cout << "Enter value to get the square root of: ";
+		getline(cin, raw);
+		try { // Test for invalid_argument.
+			a = stold(raw);
+		} catch (invalid_argument&) {
+			return "Invalid."; // invalid_argument exception? Return "Invalid".
+		}
+		c = sqrt(a); // Get square root of a.
+	}
+	return to_string(c);
+}
 
 string exponent() {
 	cout << "Enter base: "; // Prompt for input of base.
@@ -16,13 +49,13 @@ string exponent() {
 		return "Invalid."; // invalid_argument exception? Return "Invalid".
 	}
 	cout << "Enter exponent: ";
-	getline(cin, raw); // Prompt for input of expoonent.
+	getline(cin, raw); // Prompt for input of exponent.
 	try { // Test for invalid_argument.
 		b = stold(raw);
 	} catch (invalid_argument&) {
 		return "Invalid."; // invalid_argument exception? Return "Invalid".
 	}
-	c = powl(a, b);
+	c = powl(a, b); // Raise a to the power of b.
 	return to_string(c);
 }
 
@@ -54,16 +87,24 @@ int main() {
 	string out;
 	do {
 		cout << "Calculator version: " << VERSION << endl; // Print version info.
-		cout << "Enter operator {+,-,*,/,^} or q to quit:"; // Prompt for operator input.
+		cout << "Enter operator to use (m for manual, q for quit): "; // Prompt for operator input.
 		getline(cin, raw);
-		if (raw.find_first_not_of("+-*/^q") != string::npos) { // Validate operator input.
+		if (raw.find_first_not_of("+-*/^qr23m") != string::npos) { // Validate operator input.
 			cout << "Invalid.\n"; // Invalid? Go to start.
 			continue;
-		} else if (raw == "^") {
+		} else if (raw.empty()){
+			continue;
+		} else if (raw == "^") { // If '^' is given, call exponent function.
 			out = exponent() + "\n";
+		} else if (raw == "r2") { // If 'r2' is given, call square root function.
+			out = root(false) + "\n";
+		} else if (raw == "r3") { // If 'r3' is given, call cubic root function.
+			out = root(true) + "\n";
+		} else if (raw == "m") { // If 'm' is given, call manual.
+			out = manual;
 		} else if (raw == "q") { // If 'q' is given, exit with code 0.
 			return 0;
-		} else {
+		} else if (raw == "+" || "-" || "*" || "/") { // Otherwise, call basic math function.
 			out = basicMath(raw[0]) + "\n";
 		}
 		cout << endl << out << endl;
